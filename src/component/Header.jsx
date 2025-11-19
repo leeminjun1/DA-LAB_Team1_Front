e18efdf497ef94f2891c2fbc82fa9e57835946a9
elroi773 <s2457@e-mirim.hs.kr>
2025-11-19 11:53:30 +0900

    회원가입

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,20 +1,25 @@
 import Exit from "../assets/exit.png";
 import Clover_Btn from "../assets/clover_btn.png";
 import "./Header.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const goToMain = () => {
     navigate("/main");
   };
   const goToLookbook = () =>{
     navigate("/look-book");
   };
+
+  const isLookBookPage = location.pathname === "/look-book";
+  
   return (
     <div className="header-grid">
       <img src={Exit} alt="exit icon" className="exit" onClick={goToMain} />
-      <img src={Clover_Btn} alt="cloverbtn icon" className="cloverbtn" onClick={goToLookbook}/>
+      {!isLookBookPage && (<img src = {Clover_Btn} alt = "cloverbtn icon" className = "cloverbtn" onClick = {goToLookbook} />)}
     </div>
   );
 }
