48c0bb0504fe56ddc30c8a6b1e2b27db0d378718
elroi773 <s2457@e-mirim.hs.kr>
2025-11-19 10:25:18 +0900

    navigate 추가

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,20 @@
 import Exit from "../assets/exit.png";
 import Clover_Btn from "../assets/clover_btn.png";
-import './Header.css';
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate("/main");
+  };
+  const goToLookbook = () =>{
+    navigate("/look-book");
+  };
   return (
-    <div className = "header-grid">
-      <img src={Exit} alt="exit icon" className="exit" />
-      <img src={Clover_Btn} alt="cloverbtn icon" className="cloverbtn" />
-      
+    <div className="header-grid">
+      <img src={Exit} alt="exit icon" className="exit" onClick={goToMain} />
+      <img src={Clover_Btn} alt="cloverbtn icon" className="cloverbtn" onClick={goToLookbook}/>
     </div>
   );
 }
