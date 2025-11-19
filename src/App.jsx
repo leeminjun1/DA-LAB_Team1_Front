171cfeff949a3821ff07969546ef329bc3e75258
elroi773 <s2457@e-mirim.hs.kr>
2025-11-19 13:43:37 +0900

    Reciever 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Intro from './pages/Intro'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
 import { Routes, Route } from 'react-router-dom'
 import SpaceMain from './pages/SpaceMain'
 import ReceiverMain from './pages/ReceiverMain'
@@ -14,9 +11,6 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/main" element={<SpaceMain />} />
         <Route path="/receiver-main" element={<ReceiverMain />} />
         <Route path="/giver-main" element={<GiverMain />} />
