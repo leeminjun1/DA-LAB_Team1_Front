d4b3666f034fea7163983f6b3054040bed408171
elroi773 <s2457@e-mirim.hs.kr>
2025-11-19 13:34:06 +0900

    Giver 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Intro from './pages/Intro'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
 import { Routes, Route } from 'react-router-dom'
-import SpaceMain from './pages/SpaceMain'
-import ReceiverMain from './pages/ReceiverMain'
+// import ReceiverMain from './pages/ReceiverMain'
 import GiverMain from './pages/GiverMain'
-import LookBook from './pages/LookBook'
-
+// import LookBook from './pages/LookBook'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/main" element={<SpaceMain />} />
-        <Route path="/receiver-main" element={<ReceiverMain />} />
+        <Route path="/" element={<GiverMain />} />
         <Route path="/giver-main" element={<GiverMain />} />
-        <Route path='/look-book' element={<LookBook />} />
-    </Routes>
+        {/* <Route path='/look-book' element={<LookBook />} /> */}
+      </Routes>
     </>
   )
 }
